5f857cf74882b236b362ff562706714d12277a71
Chris Bonifacio <christopher.bonifacio@gmail.com>
2021-03-30 14:37:01 -0700

    reproduce issue where using user pool id returned 403

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Amplify, { Auth, Storage } from "aws-amplify";
+import {
+  AmplifyAuthenticator,
+  AmplifyS3ImagePicker,
+  AmplifySignOut,
+} from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
 
-function App() {
-  return (
+Amplify.configure(awsconfig);
+
+const AuthStateApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+  const [result, setResult] = React.useState("");
+
+  const handleUpload = async (type = "user-pool") => {
+    let id = "";
+    if (type === "user-pool") id = user.username;
+
+    if (type === "identity-pool") id = user.identityId;
+
+    try {
+      await Storage.put("words.txt", "Hello from amplify", {
+        level: "protected",
+        identityId: id,
+      });
+      setResult("Success!");
+    } catch (error) {
+      setResult("Failed.");
+    }
+  };
+
+  React.useEffect(() => {
+    return onAuthUIStateChange(async (nextAuthState, authData) => {
+      if (!user) setResult("");
+      const creds = await Auth.currentCredentials();
+      setAuthState(nextAuthState);
+      setUser({ ...authData, ...creds });
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Hello, {user.username}</div>
+      <AmplifySignOut />
+      <button onClick={() => handleUpload("user-pool")}>
+        Upload File with User Pool ID
+      </button>
+      <button onClick={() => handleUpload("identity-pool")}>
+        Upload File with Identity Pool ID
+      </button>
+      <h2>Result: {result}</h2>
     </div>
+  ) : (
+    <AmplifyAuthenticator />
   );
-}
+};
 
-export default App;
+export default AuthStateApp;
